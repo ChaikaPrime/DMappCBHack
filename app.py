9329059fc2fb0ce0ba0885cd3a3f0169593c2389
Igor Romanov <igor.romanov.ai@gmail.com>
2019-03-09 22:34:43 +0000

    Added posting of the path coordinates to the server

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+import json
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,5 +16,22 @@ def get_path():
     with open("path.json", "r") as f:
         return f.read()
 
+@app.route('/submitRoute', methods = ['POST'])
+def user():
+    if request.method == 'POST':
+        data = request.form # a multidict containing POST data
+        #print(data)
+        #print(data)
+        keys = list(data.to_dict().keys())
+        d = json.loads(keys[0])
+        print(d['routeName'])
+        print(d['newPathPoints'])
+        with open("path.json", "a+") as f:
+            return f.write()
+    else:
+        # POST Error 405 Method Not Allowed
+        pass
+    return 'ok!'
 
-app.run()
+if __name__ == '__main__':
+    app.run()
